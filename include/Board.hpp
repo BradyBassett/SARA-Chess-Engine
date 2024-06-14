936c7e859bd2d1dbe0d7c5e72e5e6e648c0dbdfb
BradyBassett <bradydbassett@gmail.com>
2024-06-14 15:30:01 -0500

    chore: Start board class

--- include/Board.hpp
+++ include/Board.hpp
@@ -1,14 +1,45 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
+#include <array>
+#include <string>
+#include <optional>
+
+#include "Bitboard.hpp"
+#include "enums/PieceType.hpp"
+#include "enums/Color.hpp"
+#include "structs/Position.hpp"
+
 class Board
 {
 public:
+	Board(std::string fenPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", std::string fenEnPassantTargetSquare = "-");
 
-protected:
+	Bitboard getPieceBitboard(PieceType piece, Color color) const;
+	void setPieceBitboard(PieceType piece, Color color, Bitboard bitboard);
+	std::optional<Position> getEnPassantTargetSquare() const;
+	void setEnPassantTargetSquare(Position enPassantTargetSquare);
+
+	// Bitboard getPawnAttackTable(Position position, Color color) const;
+	// Bitboard getKnightAttackTable(Position position) const;
+	// Bitboard getBishopAttackTable(Position position) const;
+	// Bitboard getRookAttackTable(Position position) const;
+	// Bitboard getQueenAttackTable(Position position) const;
+	// Bitboard getKingAttackTable(Position position) const;
 
 private:
+	std::array<std::array<Bitboard, 6>, 2> pieceBitboards;
+	std::array<std::array<Bitboard, 64>, 2> pawnAttackTable;
+	std::array<Bitboard, 64> knightAttackTable;
+	std::array<Bitboard, 64> bishopAttackTable;
+	std::array<Bitboard, 64> rookAttackTable;
+	std::array<Bitboard, 64> queenAttackTable;
+	std::array<Bitboard, 64> kingAttackTable;
+	std::optional<Position> enPassantTargetSquare;
 
+	void parseFenPosition(std::string fenPosition);
+	void parseFenEnPassantTargetSquare(std::string fenEnPassantTargetSquare);
+	Position convertStringToPosition(std::string position);
 };
 
 #endif // BOARD_HPP
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,2 +1,87 @@
-// #include "Board.hpp"
+#include "../include/Board.hpp"
+#include "../include/enums/PieceType.hpp"
 
+#include <map>
+
+Board::Board(std::string fenPosition, std::string fenEnPassantTargetSquare)
+{
+	parseFenPosition(fenPosition);
+	parseFenEnPassantTargetSquare(fenEnPassantTargetSquare);
+}
+
+Bitboard Board::getPieceBitboard(PieceType piece, Color color) const
+{
+	return pieceBitboards[color][piece];
+}
+
+void Board::setPieceBitboard(PieceType piece, Color color, Bitboard bitboard)
+{
+	pieceBitboards[color][piece] = bitboard;
+}
+
+std::optional<Position> Board::getEnPassantTargetSquare() const
+{
+	return enPassantTargetSquare;
+}
+
+void Board::setEnPassantTargetSquare(Position enPassantTargetSquare)
+{
+	this->enPassantTargetSquare = enPassantTargetSquare;
+}
+
+void Board::parseFenPosition(std::string fenPosition)
+{
+	int rowIndex = 0;
+	int colIndex = 0;
+
+	std::map<char, PieceType> charToPiece = {
+		{'p', PieceType::PAWN},
+		{'n', PieceType::KNIGHT},
+		{'b', PieceType::BISHOP},
+		{'r', PieceType::ROOK},
+		{'q', PieceType::QUEEN},
+		{'k', PieceType::KING}
+	};
+
+	for (char c : fenPosition)
+	{
+		if (c == '/')
+		{
+			rowIndex++;
+			colIndex = 0;
+		}
+		else if (isdigit(c))
+		{
+			colIndex += c - '0';
+		}
+		else
+		{
+			Color color = isupper(c) ? Color::WHITE : Color::BLACK;
+			PieceType piece = charToPiece[tolower(c)];
+
+			Position position{rowIndex, colIndex};
+			Bitboard temp1 = getPieceBitboard(piece, color);
+			Bitboard temp2 = Bitboard(position);
+			Bitboard temp3 = temp1 | temp2;
+			setPieceBitboard(piece, color, getPieceBitboard(piece, color) | Bitboard(position));
+			colIndex++;
+		}
+	}
+}
+
+void Board::parseFenEnPassantTargetSquare(std::string fenEnPassantTargetSquare)
+{
+	if (fenEnPassantTargetSquare == "-")
+	{
+		enPassantTargetSquare = std::nullopt;
+	}
+	else
+	{
+		enPassantTargetSquare = convertStringToPosition(fenEnPassantTargetSquare);
+	}
+}
+
+Position Board::convertStringToPosition(std::string position)
+{
+	return Position{8 - (position[1] - '0'), position[0] - 'a'};
+}
