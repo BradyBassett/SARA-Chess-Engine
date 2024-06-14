e3e91c05abfd16964cc48a69548cc5b09fee467d
BradyBassett <bradydbassett@gmail.com>
2024-06-14 15:29:45 -0500

    chore: Refactor Game class constructor and parsing methods

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,14 +1,49 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+#include "Board.hpp"
+#include "enums/Color.hpp"
+#include "structs/Move.hpp"
+#include "structs/CastleRights.hpp"
+
 class Game
 {
 public:
+	Game(std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+	Game(std::vector<std::string> fenParts);
 
-protected:
+	Color getActiveColor() const;
+	void setActiveColor(Color activeColor);
+	int getHalfMoveClock() const;
+	void setHalfMoveClock(int halfMoveClock);
+	int getFullMoveNumber() const;
+	void setFullMoveNumber(int fullMoveNumber);
+	CastleRights getWhiteCastleRights() const;
+	CastleRights getBlackCastleRights() const;
+
+	void switchActiveColor(Color color);
+	void incrementHalfMoveClock();
+	void incrementFullMoveNumber();
 
 private:
+	Color activeColor;
+	Board board;
+	std::vector<Move> moveHistory;
+	std::unordered_map<std::string, int> gameStateHistory;
+	int halfMoveClock;
+	int fullMoveNumber;
+	CastleRights whiteCastleRights;
+	CastleRights blackCastleRights;
 
+	std::vector<std::string> getFenTokens(std::string fen);
+	void parseActiveColor(std::string color);
+	void parsehalfMoveClock(std::string halfMoveClock);
+	void parseFullMoveNumber(std::string fullMoveNumber);
+	void parseCastlingRights(std::string castlingRights);
 };
 
 #endif // GAME_HPP
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,2 +1,116 @@
-// #include "Game.hpp"
+#include "../include/Game.hpp"
 
+#include <sstream>
+
+Game::Game(std::string fen) : Game(getFenTokens(fen))
+{
+}
+
+Game::Game(std::vector<std::string> fenParts) : board(fenParts[0], fenParts[3])
+{
+	parseActiveColor(fenParts[1]);
+	parseCastlingRights(fenParts[2]);
+	parsehalfMoveClock(fenParts[4]);
+	parseFullMoveNumber(fenParts[5]);
+}
+
+Color Game::getActiveColor() const
+{
+	return activeColor;
+}
+
+void Game::setActiveColor(Color activeColor)
+{
+	this->activeColor = activeColor;
+}
+
+int Game::getHalfMoveClock() const
+{
+	return halfMoveClock;
+}
+
+void Game::setHalfMoveClock(int halfMoveClock)
+{
+	this->halfMoveClock = halfMoveClock;
+}
+
+int Game::getFullMoveNumber() const
+{
+	return fullMoveNumber;
+}
+
+void Game::setFullMoveNumber(int fullMoveNumber)
+{
+	this->fullMoveNumber = fullMoveNumber;
+}
+
+CastleRights Game::getWhiteCastleRights() const
+{
+	return whiteCastleRights;
+}
+
+CastleRights Game::getBlackCastleRights() const
+{
+	return blackCastleRights;
+}
+
+void Game::switchActiveColor(Color color)
+{
+	color == Color::WHITE ? activeColor = Color::BLACK : activeColor = Color::WHITE;
+}
+
+void Game::incrementHalfMoveClock()
+{
+	halfMoveClock++;
+}
+
+void Game::incrementFullMoveNumber()
+{
+	fullMoveNumber++;
+}
+
+std::vector<std::string> Game::getFenTokens(std::string fen)
+{
+	std::vector<std::string> parts;
+	std::istringstream iss(fen);
+	std::string part;
+	while (std::getline(iss, part, ' '))
+	{
+		parts.push_back(part);
+	}
+
+	return parts;
+}
+
+void Game::parseActiveColor(std::string color)
+{
+	if (color == "w")
+	{
+		activeColor = Color::WHITE;
+	}
+	else
+	{
+		activeColor = Color::BLACK;
+	}
+}
+
+void Game::parsehalfMoveClock(std::string halfMoveClock)
+{
+	this->halfMoveClock = std::stoi(halfMoveClock);
+}
+
+void Game::parseFullMoveNumber(std::string fullMoveNumber)
+{
+	this->fullMoveNumber = std::stoi(fullMoveNumber);
+}
+
+void Game::parseCastlingRights(std::string castlingRights)
+{
+    bool whiteCanCastleKingside = castlingRights.find('K') != std::string::npos;
+    bool whiteCanCastleQueenside = castlingRights.find('Q') != std::string::npos;
+    bool blackCanCastleKingside = castlingRights.find('k') != std::string::npos;
+    bool blackCanCastleQueenside = castlingRights.find('q') != std::string::npos;
+
+    whiteCastleRights = CastleRights(whiteCanCastleKingside, whiteCanCastleQueenside);
+    blackCastleRights = CastleRights(blackCanCastleKingside, blackCanCastleQueenside);
+}
