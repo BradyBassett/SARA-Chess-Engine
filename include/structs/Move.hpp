2e8cbfde7c8b9245e4f2cc1fca2fd5e05d2b7a7c
BradyBassett <bradydbassett@gmail.com>
2024-06-14 20:21:11 -0500

    Add fields to move struct

--- include/structs/Move.hpp
+++ include/structs/Move.hpp
@@ -1,10 +1,27 @@
 #ifndef MOVE_HPP
 #define MOVE_HPP
 
+#include <optional>
+
+#include "Position.hpp"
+#include "CastleRights.hpp"
+#include "../enums/Color.hpp"
+#include "../enums/PieceType.hpp"
+#include "../enums/SpecialMove.hpp"
+
 struct Move
 {
-	/* data */
+	Position from;
+	Position to;
+	PieceType PieceType;
+	Color color;
+	std::optional<PieceType> capturedPiece;
+	std::optional<Position> enPassantTargetSquare;
+	SpecialMove specialMove;
+	CastleRights whiteCastleRights;
+	CastleRights blackCastleRights;
+	int halfMoveClock;
+	int fullMoveNumber;
 };
-
 
 #endif // MOVE_HPP
