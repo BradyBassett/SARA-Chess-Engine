b70deefa800627a22d5fb1f59f751c9acd0eba1d
BradyBassett <bradydbassett@gmail.com>
2024-06-14 19:23:04 -0500

    implemented beginings of simple game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
-#include "../include/Bitboard.hpp"
+#include "../include/Game.hpp"
 
 int main();
 
 int main()
 {
-	Bitboard bitboard = Bitboard();
+	std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	std::cout << "Enter Fen (or press enter for default): ";
+	std::string input;
+	std::getline(std::cin, input);
+	if (!input.empty())
+	{
+		fen = input;
+	}
 
-	std::cout << "Bitboard value: " << bitboard.getValue() << std::endl;
+	Game game(fen);
+
+	bool running = true;
+	while (running)
+	{
+		std::cout << game.getBoard().boardToAscii() << std::endl;
+
+		std::string line, from, to, promotion;
+
+		std::cout << (game.getActiveColor() == Color::WHITE ? "White" : "Black") << " - Enter move: ";
+		std::getline(std::cin, line);
+		std::istringstream iss(line);
+		iss >> from >> to >> promotion;
+
+		if (from == "quit")
+		{
+			running = false;
+			continue;
+		}
+
+		Position fromPos = game.getBoard().convertStringToPosition(from);
+		Position toPos = game.getBoard().convertStringToPosition(to);
+
+		try
+		{
+			std::system("clear");
+			// game.makeMove(fromPos, toPos, promotion);
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << e.what() << "\n\n";
+			continue;
+		}
+
+		game.switchActiveColor();
+	}
+
+	std::cout << game.getBoard().boardToAscii() << std::endl;
 
 	return 0;
 }
