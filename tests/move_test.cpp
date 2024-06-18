cfb58459dbcec83ba44233919f87ddf62d5dcb43
BradyBassett <bradydbassett@gmail.com>
2024-06-18 14:37:49 -0500

    Refactor move_test.cpp to include additional test cases

--- tests/move_test.cpp
+++ tests/move_test.cpp
@@ -51,7 +51,9 @@ const auto moveConstructorTestParams = ::testing::Values(
 	MoveConstructorTestParams{Position{7, 7}, Position{7, 6}, PieceType::BISHOP, Color::WHITE, PieceType::PAWN, std::nullopt, SpecialMove::EN_PASSANT, CastleRights{true, true}, CastleRights{false, false}, 100, 1000},
 	MoveConstructorTestParams{Position{3, 3}, Position{3, 4}, PieceType::ROOK, Color::BLACK, PieceType::KNIGHT, Position{2, 4}, SpecialMove::KINGSIDE_CASTLE, CastleRights{true, false}, CastleRights{true, true}, 50, 5899},
 	MoveConstructorTestParams{Position{5, 5}, Position{5, 6}, PieceType::QUEEN, Color::WHITE, PieceType::BISHOP, Position{4, 6}, SpecialMove::QUEENSIDE_CASTLE, CastleRights{false, true}, CastleRights{true, false}, 99, 356},
-	MoveConstructorTestParams{Position{6, 6}, Position{6, 7}, PieceType::KING, Color::BLACK, PieceType::ROOK, Position{7, 7}, SpecialMove::PROMOTION, CastleRights{false, false}, CastleRights{false, false}, 1, 2}
+	MoveConstructorTestParams{Position{6, 6}, Position{6, 7}, PieceType::KING, Color::BLACK, PieceType::ROOK, Position{7, 7}, SpecialMove::PROMOTION, CastleRights{false, false}, CastleRights{false, false}, 1, 2},
+	MoveConstructorTestParams{Position{2, 2}, Position{2, 3}, PieceType::PAWN, Color::WHITE, PieceType::QUEEN, std::nullopt, SpecialMove::NONE, CastleRights{true, true}, CastleRights{true, true}, 0, 1},
+	MoveConstructorTestParams{Position{4, 4}, Position{4, 5}, PieceType::KNIGHT, Color::BLACK, PieceType::KING, Position{4, 6}, SpecialMove::DOUBLE_PAWN_PUSH, CastleRights{false, true}, CastleRights{true, true}, 4, 10}
 );
 
 INSTANTIATE_TEST_SUITE_P(MoveConstructorTest, MoveConstructorTest, moveConstructorTestParams);
