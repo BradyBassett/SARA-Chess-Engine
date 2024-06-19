db1dee21e3deca23b7d3faae7d9eb964e216c9c2
BradyBassett <bradydbassett@gmail.com>
2024-06-19 17:28:58 -0500

    temp values

--- include/Piece.hpp
+++ include/Piece.hpp
@@ -1,14 +1,16 @@
 #ifndef PIECE_HPP
 #define PIECE_HPP
 
+#include "../include/enums/PieceType.hpp"
+#include "../include/enums/Color.hpp"
+
 class Piece
 {
 public:
 
-protected:
-
 private:
-
+	PieceType pieceType;
+	Color color;
 };
 
 #endif // PIECE_HPP
