3225f03f7896aa691f41da1ce4986f8069a8837c
BradyBassett <bradydbassett@gmail.com>
2024-06-20 16:25:48 -0500

    Init, removed unused files

---
+++ include/MoveGenerator.hpp
@@ -0,0 +1,9 @@
+#ifndef MOVE_GENERATOR_HPP
+#define MOVE_GENERATOR_HPP
+
+#include "../include/enums/PieceType.hpp"
+#include "../include/enums/Color.hpp"
+
+
+
+#endif // MOVE_GENERATOR_HPP
---
+++ src/MoveGenerator.cpp
@@ -0,0 +1,2 @@
+// #include "MoveGenerator.hpp"
+
