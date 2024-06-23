75f8a74c2f35d1ae322f441baef3b47ecc89d835
BradyBassett <bradydbassett@gmail.com>
2024-06-22 21:21:02 -0500

    Added test cases for valid validateMove tests

---
+++ tests/moveValidator_test.cpp
@@ -0,0 +1,104 @@
+#include <gtest/gtest.h>
+
+#include "../include/MoveValidator.hpp"
+#include "../include/Utility.hpp"
+
+struct ValidValidateMoveTestParams
+{
+	std::string fen;
+	Position from;
+	Position to;
+	PieceType piece;
+	Color color;
+};
+
+class validValidateMoveTest : public ::testing::TestWithParam<ValidValidateMoveTestParams> {};
+
+TEST_P(validValidateMoveTest, test)
+{
+	auto params = GetParam();
+	Game game(params.fen);
+	EXPECT_NO_THROW(MoveValidator::validateMove(params.from, params.to, params.piece, params.color, game));
+}
+
+const auto validValidateMoveTestParams = ::testing::Values(
+	// Valid pawn move
+	ValidValidateMoveTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Position{Utility::convertStringToPosition("e2")}, Position{Utility::convertStringToPosition("e3")}, PieceType::PAWN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Position{Utility::convertStringToPosition("e2")}, Position{Utility::convertStringToPosition("e4")}, PieceType::PAWN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp2ppp/3p4/4p3/4P3/3P4/PPP2PPP/RNBQKBNR w KQkq - 0 3", Position{Utility::convertStringToPosition("d3")}, Position{Utility::convertStringToPosition("d4")}, PieceType::PAWN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp2ppp/8/3pp3/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 4", Position{Utility::convertStringToPosition("d4")}, Position{Utility::convertStringToPosition("e5")}, PieceType::PAWN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp3pp/8/4Pp2/4p3/2P5/PP3PPP/RNBQKBNR w KQkq f6 0 6", Position{Utility::convertStringToPosition("e5")}, Position{Utility::convertStringToPosition("f6")}, PieceType::PAWN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", Position{Utility::convertStringToPosition("e7")}, Position{Utility::convertStringToPosition("e6")}, PieceType::PAWN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", Position{Utility::convertStringToPosition("e7")}, Position{Utility::convertStringToPosition("e5")}, PieceType::PAWN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp2ppp/3p4/4p3/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 3", Position{Utility::convertStringToPosition("d6")}, Position{Utility::convertStringToPosition("d5")}, PieceType::PAWN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp2ppp/8/3pP3/4P3/8/PPP2PPP/RNBQKBNR b KQkq - 0 4", Position{Utility::convertStringToPosition("d5")}, Position{Utility::convertStringToPosition("e4")}, PieceType::PAWN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnbqkbnr/pp4pp/2p2P2/8/4pP2/2P5/PP4PP/RNBQKBNR b KQkq f3 0 7", Position{Utility::convertStringToPosition("e4")}, Position{Utility::convertStringToPosition("f3")}, PieceType::PAWN, Color::BLACK},
+	// Valid knight move
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp3pp/8/4Pp2/4p3/2P5/PP3PPP/RNBQKBNR w KQkq f6 0 6", Position{Utility::convertStringToPosition("g1")}, Position{Utility::convertStringToPosition("f3")}, PieceType::KNIGHT, Color::WHITE},
+	ValidValidateMoveTestParams{"rnb2b1r/ppp3pp/5nk1/3QPp2/3qp3/2P2N2/PP3PPP/RNB1KB1R w KQ - 8 10", Position{Utility::convertStringToPosition("f3")}, Position{Utility::convertStringToPosition("d4")}, PieceType::KNIGHT, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbqkbnr/ppp3pp/8/4Pp2/4p3/2P2N2/PP3PPP/RNBQKB1R b KQkq - 1 6", Position{Utility::convertStringToPosition("g8")}, Position{Utility::convertStringToPosition("f6")}, PieceType::KNIGHT, Color::BLACK},
+	ValidValidateMoveTestParams{"rnb2b1r/ppp3pp/5nk1/3QPp2/3Np3/2P5/PP3PPP/RNB1KB1R b KQ - 0 10", Position{Utility::convertStringToPosition("f6")}, Position{Utility::convertStringToPosition("d5")}, PieceType::KNIGHT, Color::BLACK},
+	// Valid bishop move
+	ValidValidateMoveTestParams{"rnb2b1r/ppp3pp/6k1/3nPp2/3Np3/2P5/PP3PPP/RNB1KB1R w KQ - 0 11", Position{Utility::convertStringToPosition("f1")}, Position{Utility::convertStringToPosition("c4")}, PieceType::BISHOP, Color::WHITE},
+	ValidValidateMoveTestParams{"rnb4r/ppp3pp/6k1/2bnPp2/2BNp3/2P5/PP3PPP/RNB1K2R w KQ - 2 12", Position{Utility::convertStringToPosition("c4")}, Position{Utility::convertStringToPosition("d5")}, PieceType::BISHOP, Color::WHITE},
+	ValidValidateMoveTestParams{"rnb2b1r/ppp3pp/6k1/3nPp2/2BNp3/2P5/PP3PPP/RNB1K2R b KQ - 1 11", Position{Utility::convertStringToPosition("f8")}, Position{Utility::convertStringToPosition("c5")}, PieceType::BISHOP, Color::BLACK},
+	ValidValidateMoveTestParams{"rnb4r/ppp3pp/6k1/2bBPp2/3Np3/2P5/PP3PPP/RNB1K2R b KQ - 0 12", Position{Utility::convertStringToPosition("c5")}, Position{Utility::convertStringToPosition("d4")}, PieceType::BISHOP, Color::BLACK},
+	// Valid rook move
+	ValidValidateMoveTestParams{"1nb4r/2p3pp/r1B3k1/pp2Pp2/P3p3/R1b5/1P1B1PPP/1N2K2R w K b6 0 17", Position{Utility::convertStringToPosition("a3")}, Position{Utility::convertStringToPosition("b3")}, PieceType::ROOK, Color::WHITE},
+	ValidValidateMoveTestParams{"1nb4r/2p3pp/1rB3k1/pp2Pp2/P3p3/1Rb5/1P1B1PPP/1N2K2R w K - 2 18", Position{Utility::convertStringToPosition("b3")}, Position{Utility::convertStringToPosition("c3")}, PieceType::ROOK, Color::WHITE},
+	ValidValidateMoveTestParams{"1nb4r/2p3pp/r1B3k1/pp2Pp2/P3p3/1Rb5/1P1B1PPP/1N2K2R b K - 1 17", Position{Utility::convertStringToPosition("a6")}, Position{Utility::convertStringToPosition("b6")}, PieceType::ROOK, Color::BLACK},
+	ValidValidateMoveTestParams{"1nb4r/2p3pp/1rB3k1/pp2Pp2/P3p3/2R5/1P1B1PPP/1N2K2R b K - 0 18", Position{Utility::convertStringToPosition("b6")}, Position{Utility::convertStringToPosition("c6")}, PieceType::ROOK, Color::BLACK},
+	// Valid queen move
+	ValidValidateMoveTestParams{"rnbqkb1r/ppp3pp/5n2/4Pp2/4p3/2P2N2/PP3PPP/RNBQKB1R w KQkq - 2 7", Position{Utility::convertStringToPosition("d1")}, Position{Utility::convertStringToPosition("a4")}, PieceType::QUEEN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnb2b1r/pp3kpp/2p2n2/q3Pp2/Q3p3/2P2NP1/PP2KP1P/RNB2B1R w - - 1 10", Position{Utility::convertStringToPosition("a4")}, Position{Utility::convertStringToPosition("d4")}, PieceType::QUEEN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnb2b1r/pp3kpp/2p2n2/3qPp2/3Qp3/2P2NP1/PP2KP1P/RNB2B1R w - - 3 11", Position{Utility::convertStringToPosition("d4")}, Position{Utility::convertStringToPosition("e4")}, PieceType::QUEEN, Color::WHITE},
+	ValidValidateMoveTestParams{"rnbq1b1r/pp3kpp/2p2n2/4Pp2/Q3p3/2P2NP1/PP2KP1P/RNB2B1R b - - 0 9", Position{Utility::convertStringToPosition("d8")}, Position{Utility::convertStringToPosition("a5")}, PieceType::QUEEN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnb2b1r/pp3kpp/2p2n2/q3Pp2/3Qp3/2P2NP1/PP2KP1P/RNB2B1R b - - 2 10", Position{Utility::convertStringToPosition("a5")}, Position{Utility::convertStringToPosition("d5")}, PieceType::QUEEN, Color::BLACK},
+	ValidValidateMoveTestParams{"rnb2b1r/pp3kpp/2p2n2/3qPp2/4Q3/2P2NP1/PP2KP1P/RNB2B1R b - - 0 11", Position{Utility::convertStringToPosition("d5")}, Position{Utility::convertStringToPosition("e5")}, PieceType::QUEEN, Color::BLACK},
+	// Valid king move
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qpp2/N1PB4/PP1B1nPP/R3K2R w KQkq - 0 14", Position{Utility::convertStringToPosition("e1")}, Position{Utility::convertStringToPosition("e2")}, PieceType::KING, Color::WHITE},
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qpp2/N1PB4/PP1B1nPP/R3K2R w KQkq - 0 14", Position{Utility::convertStringToPosition("e1")}, Position{Utility::convertStringToPosition("f2")}, PieceType::KING, Color::WHITE},
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qpp2/N1PB4/PP1B1nPP/R3K2R w KQkq - 0 14", Position{Utility::convertStringToPosition("e1")}, Position{Utility::convertStringToPosition("g1")}, PieceType::KING, Color::WHITE},
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qppn1/N1PB2P1/PP1B3P/R3K2R w KQkq - 1 15", Position{Utility::convertStringToPosition("e1")}, Position{Utility::convertStringToPosition("c1")}, PieceType::KING, Color::WHITE},
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qpp2/N1PB4/PP1BKnPP/R6R b kq - 1 14", Position{Utility::convertStringToPosition("e8")}, Position{Utility::convertStringToPosition("f7")}, PieceType::KING, Color::BLACK},
+	ValidValidateMoveTestParams{"r3k2r/pppbN1pp/2nq4/4P3/3Qpp2/N1PB4/PP1BKnPP/R6R b kq - 1 14", Position{Utility::convertStringToPosition("e8")}, Position{Utility::convertStringToPosition("e7")}, PieceType::KING, Color::BLACK},
+	ValidValidateMoveTestParams{"r3k2r/pppb2pp/2nq4/3NP3/3Qp3/N1PB1p2/PP1B1nPP/R4RK1 b kq - 1 15", Position{Utility::convertStringToPosition("e8")}, Position{Utility::convertStringToPosition("g8")}, PieceType::KING, Color::BLACK},
+	ValidValidateMoveTestParams{"r3k2r/pppb2pp/2nq4/4PN2/3Q1pn1/N1PBp1P1/PP1B3P/2KR3R b kq - 1 16", Position{Utility::convertStringToPosition("e8")}, Position{Utility::convertStringToPosition("c8")}, PieceType::KING, Color::BLACK}
+);
+
+INSTANTIATE_TEST_SUITE_P(validValidateMoveTest, validValidateMoveTest, validValidateMoveTestParams);
+
+// TODO: Implement invalidValidateMoveTest
+// struct InvalidValidateMoveTestParams
+// {
+// 	std::string fen;
+// 	Position from;
+// 	Position to;
+// 	PieceType piece;
+// 	Color color;
+// 	std::string expectedError;
+// };
+
+// class invalidValidateMoveTest : public ::testing::TestWithParam<InvalidValidateMoveTestParams> {};
+
+// TEST_P(invalidValidateMoveTest, test)
+// {
+// 	auto params = GetParam();
+// 	Game game(params.fen);
+// 	EXPECT_THROW({
+// 		try
+// 		{
+// 			MoveValidator::validateMove(params.from, params.to, params.piece, params.color, game);
+// 		}
+// 		catch (const std::invalid_argument &e)
+// 		{
+// 			EXPECT_STREQ(e.what(), params.expectedError.c_str());
+// 			throw;
+// 		}
+// 	}, std::invalid_argument);
+// }
+
+// const auto invalidValidateMoveTestParams = ::testing::Values(
+// );
+
+// INSTANTIATE_TEST_SUITE_P(invalidValidateMoveTest, invalidValidateMoveTest, invalidValidateMoveTestParams);
