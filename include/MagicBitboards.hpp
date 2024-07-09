ac687d3c417ca95f500d8b5d07bcdaf39453f244
BradyBassett <bradydbassett@gmail.com>
2024-07-09 13:36:24 -0500

    Added functionality for magic bitboards for sliding piece move generation

---
+++ include/MagicBitboards.hpp
@@ -0,0 +1,60 @@
+#ifndef MAGICBITBOARDS_HPP
+#define MAGICBITBOARDS_HPP
+
+#include "Bitboard.hpp"
+#include "enums/PieceType.hpp"
+
+#include <array>
+#include <vector>
+#include <mutex>
+
+class MagicBitboards
+{
+public:
+	static void init();
+	static Bitboard getSliderAttacks(int square, Bitboard occupied, PieceType pieceType);
+
+private:
+	static std::once_flag initFlag;
+
+	// Precomputed magic numbers for sliders
+	static std::array<uint64_t, 64> rookMagics;
+	static std::array<uint64_t, 64> bishopMagics;
+
+	// Precomputed shift values for sliders
+	static std::array<int, 64> rookShifts;
+	static std::array<int, 64> bishopShifts;
+
+	// Masks for the sliders
+	static std::array<uint64_t, 64> rookMasks;
+	static std::array<uint64_t, 64> bishopMasks;
+
+	// Attacks for the sliders
+	static std::array<std::array<uint64_t, 4096>, 64> rookAttacks;
+	static std::array<std::array<uint64_t, 512>, 64> bishopAttacks;
+
+	static Bitboard getRookAttacks(int square, Bitboard occupied);
+	static Bitboard getBishopAttacks(int square, Bitboard occupied);
+	static std::vector<uint64_t> createAllBlockerPatterns(uint64_t mask);
+	static uint64_t legalMovesFromBlockers(int square, uint64_t blockers, bool rook);
+
+	template<size_t N>
+	static std::array<uint64_t, N> createTable(int square, bool rook, uint64_t magic, int shift)
+	{
+		std::array<uint64_t, N> table = {};
+
+		uint64_t movementMask = rook ? rookMasks[square] : bishopMasks[square];
+		std::vector<uint64_t> blockerPatterns = createAllBlockerPatterns(movementMask);
+
+		for (uint64_t pattern : blockerPatterns)
+		{
+			uint64_t index = (pattern * magic) >> shift;
+			uint64_t moves = legalMovesFromBlockers(square, pattern, rook);
+			table[index] = moves;
+		}
+
+		return table;
+	}
+};
+
+#endif // MAGICBITBOARDS_HPP
---
+++ src/MagicBitboards.cpp
@@ -0,0 +1,124 @@
+#include "../include/MagicBitboards.hpp"
+#include "../include/JsonLoader.hpp"
+#include "../include/structs/Position.hpp"
+#include "../include/Utility.hpp"
+
+#include <nlohmann/json.hpp>
+
+std::once_flag MagicBitboards::initFlag;
+
+std::array<uint64_t, 64> MagicBitboards::rookMagics = {};
+std::array<uint64_t, 64> MagicBitboards::bishopMagics = {};
+std::array<int, 64> MagicBitboards::rookShifts = {};
+std::array<int, 64> MagicBitboards::bishopShifts = {};
+std::array<uint64_t, 64> MagicBitboards::rookMasks = {};
+std::array<uint64_t, 64> MagicBitboards::bishopMasks = {};
+std::array<std::array<uint64_t, 4096>, 64> MagicBitboards::rookAttacks = {};
+std::array<std::array<uint64_t, 512>, 64> MagicBitboards::bishopAttacks = {};
+
+void MagicBitboards::init()
+{
+	std::call_once(initFlag, []() {
+		loadArrayFromJson("src/data/rookMagics.json", rookMagics);
+		loadArrayFromJson("src/data/bishopMagics.json", bishopMagics);
+		loadArrayFromJson("src/data/rookShifts.json", rookShifts);
+		loadArrayFromJson("src/data/bishopShifts.json", bishopShifts);
+		loadArrayFromJson("src/data/rookMasks.json", rookMasks);
+		loadArrayFromJson("src/data/bishopMasks.json", bishopMasks);
+
+		for (int square = 0; square < 64; ++square)
+		{
+			rookAttacks[square] = createTable<4096>(square, true, rookMagics[square], rookShifts[square]);
+			bishopAttacks[square] = createTable<512>(square, false, bishopMagics[square], bishopShifts[square]);
+		}
+	});
+}
+
+Bitboard MagicBitboards::getSliderAttacks(int square, Bitboard occupied, PieceType pieceType)
+{
+	switch (pieceType)
+	{
+	case PieceType::ROOK:
+		return getRookAttacks(square, occupied);
+	case PieceType::BISHOP:
+		return getBishopAttacks(square, occupied);
+	case PieceType::QUEEN:
+		return getBishopAttacks(square, occupied) | getRookAttacks(square, occupied);
+	default:
+		return Bitboard(0);
+	}
+}
+
+Bitboard MagicBitboards::getRookAttacks(int square, Bitboard occupied)
+{
+	uint64_t index = (rookMasks[square] & occupied.getValue()) * rookMagics[square] >> rookShifts[square];
+	return rookAttacks[square][index];
+}
+
+Bitboard MagicBitboards::getBishopAttacks(int square, Bitboard occupied)
+{
+	uint64_t index = (bishopMasks[square] & occupied.getValue()) * bishopMagics[square] >> bishopShifts[square];
+	return bishopAttacks[square][index];
+}
+
+std::vector<uint64_t> MagicBitboards::createAllBlockerPatterns(uint64_t mask)
+{
+	// Create a vector containing indices of the set bits in the mask
+	std::vector<int> maskBits = {};
+	for (int square = 0; square < 64; square++)
+	{
+		if (((mask >> square) & 1) == 1)
+		{
+			maskBits.push_back(square);
+		}
+	}
+
+	int numPatterns = 1 << maskBits.size();
+	std::vector<uint64_t> blockerBitboards;
+	blockerBitboards.resize(numPatterns, 0);
+
+	for (int patternIndex = 0; patternIndex < numPatterns; patternIndex++)
+	{
+		for (int bitIndex = 0; bitIndex < maskBits.size(); bitIndex++)
+		{
+			int bit = (patternIndex >> bitIndex) & 1;
+			blockerBitboards[patternIndex] |= (static_cast<u_int64_t>(bit) << maskBits[bitIndex]);
+		}
+	}
+
+	return blockerBitboards;
+}
+
+uint64_t MagicBitboards::legalMovesFromBlockers(int square, uint64_t blockers, bool rook)
+{
+	Bitboard moves;
+	std::array<Position, 4> directions = rook ?
+		std::array<Position, 4>{{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}} :
+		std::array<Position, 4>{{{1, 1}, {-1, -1}, {1, -1}, {-1, 1}}};
+	Position startPos = Utility::calculatePosition(square);
+
+	for (Position direction : directions)
+	{
+		for (int distance = 1; distance < 8; distance++)
+		{
+			Position targetPos = Position{startPos.row + (direction.row * distance), startPos.col + (direction.col * distance)};
+			try
+			{
+				Utility::validatePosition(targetPos);
+			}
+			catch(const std::exception& e)
+			{
+				break;
+			}
+
+			// If target is valid, set the bit in the moves bitboard, and check if there is a blocker at the target position
+			moves.setBit(targetPos);
+			if (Bitboard(blockers).getBit(targetPos))
+			{
+				break;
+			}
+		}
+	}
+
+	return moves.getValue();
+}
