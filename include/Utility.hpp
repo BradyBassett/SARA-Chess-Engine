7e749e75d17515259ace539edd23f4913a6fa4ea
BradyBassett <bradydbassett@gmail.com>
2024-07-09 17:59:40 -0500

    chore: Move loadArrayFromJson template to Utility.hpp

--- include/Utility.hpp
+++ include/Utility.hpp
@@ -3,6 +3,10 @@
 
 #include "structs/Position.hpp"
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <array>
+#include <iostream>
 #include <string>
 
 namespace Utility {
@@ -12,6 +16,24 @@ namespace Utility {
     void validateSquareNumber(int squareNumber);
     Position convertStringToPosition(std::string str);
     std::string convertPositionToString(Position position);
+
+    using json = nlohmann::json;
+    template<typename T, size_t Size>
+    static void loadArrayFromJson(std::string filepath, std::array<T, Size>& arr) {
+        std::ifstream file(filepath);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << filepath << std::endl;
+            return;
+        }
+
+        json j;
+        file >> j;
+
+        for (size_t i = 0; i < Size; ++i) {
+            T it = j.at(i).get<T>();
+            arr[i] = j.at(i).get<T>();
+        }
+    }
 }
 
 #endif // UTILITY_HPP
