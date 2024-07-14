741ccfef9a12e1122fa1bc8c94480858eba72295
BradyBassett <bradydbassett@gmail.com>
2024-07-13 23:03:58 -0500

    Added generatePotentialMoves methods to MoveValidator

--- include/MoveValidator.hpp
+++ include/MoveValidator.hpp
@@ -10,7 +10,11 @@ public:
 	static bool calculateInCheck();
 
 private:
-	static bool isPathClear(Position from, Position to, PieceType piece, Color color, Game &game);
+	static Bitboard generatePotentialMoves(Position position, PieceType piece, Color color, Board &board);
+	static Bitboard generatePotentialPawnMoves(Color color, Position position, Bitboard moves);
+	static bool canDoublePawnPush(Color color, Position position);
+	static Bitboard generatePotentialKingMoves(Color color, Position position, Bitboard moves);
+	static bool isPathClear(Position from, Position to, PieceType piece, Color color, Board &board);
 	static Bitboard calculatePath(Position from, Position to, PieceType piece, Color color);
 	static Bitboard calculatePawnPath(Position from, Position to, Color color);
 	static Bitboard calculateDiagonalPath(Position from, Position to);
@@ -18,12 +22,12 @@ private:
 	static bool isDiagonal(Position from, Position to);
 	static bool isOrthogonal(Position from, Position to);
 
-	static void validatePawnMove(Position from, Position to, PieceType piece, Color color, Game &game);
+	static void validatePawnMove(Position from, Position to, PieceType piece, Color color, Board &board);
 	static void validateKingMove(Position from, Position to, PieceType piece, Color color, Game &game);
 	static void validateCastlingMove(Position from, Position to, Color color, Game &game);
 	static bool isValidRookPosition(Position from, Color color, Game &game);
 
-	static bool isSquareAttacked(Position position, Color color, Game &game);
+	static bool isSquareAttacked(Position position, Color color, Board &board);
 };
 
 #endif // MOVEVALIDATOR_HPP
--- src/MoveValidator.cpp
+++ src/MoveValidator.cpp
@@ -1,11 +1,12 @@
 #include "../include/MoveValidator.hpp"
-#include "../include/MoveGenerator.hpp"
 #include "../include/Utility.hpp"
 
 #include <stdexcept>
 
 void MoveValidator::validateMove(Position from, Position to, PieceType piece, Color color, Game &game)
 {
+	Board &board = game.getBoard();
+
 	// Check if the piece is of the correct color
 	if (color != game.getActiveColor())
 	{
@@ -19,19 +20,19 @@ void MoveValidator::validateMove(Position from, Position to, PieceType piece, Co
 	}
 
 	// Check if the from square is occupied by a piece of the correct color
-	if (game.getBoard().getColorBitboard(color).getBit(from) == 0)
+	if (board.getColorBitboard(color).getBit(from) == 0)
 	{
 		throw std::invalid_argument("Invalid move - There is no piece on this square");
 	}
 
 	// Check if the to square is occupied by a piece of the same color
-	if (game.getBoard().getColorBitboard(color).getBit(to) != 0)
+	if (board.getColorBitboard(color).getBit(to) != 0)
 	{
 		throw std::invalid_argument("Invalid move - You cannot capture your own piece");
 	}
 
 	// Check if the to square is in the list of potential moves
-	if (MoveGenerator::generatePotentialMoves(piece, color, from).getBit(to) == 0)
+	if (generatePotentialMoves(from, piece, color, board).getBit(to) == 0)
 	{
 		throw std::invalid_argument("Invalid move - The piece cannot move to the to square");
 	}
@@ -41,7 +42,7 @@ void MoveValidator::validateMove(Position from, Position to, PieceType piece, Co
 	if (piece != PieceType::KNIGHT && piece != PieceType::KING)
 	{
 		// Check if the path is clear for all pieces except knights and kings
-		if (!isPathClear(from, to, piece, color, game))
+		if (!isPathClear(from, to, piece, color, board))
 		{
 			throw std::invalid_argument("Invalid move - The path is not clear");
 		}
@@ -50,7 +51,7 @@ void MoveValidator::validateMove(Position from, Position to, PieceType piece, Co
 	if (piece == PieceType::PAWN)
 	{
 		// Pawn-specific move validation
-		validatePawnMove(from, to, piece, color, game);
+		validatePawnMove(from, to, piece, color, board);
 	}
 	else if (piece == PieceType::KING)
 	{
@@ -64,10 +65,75 @@ bool MoveValidator::calculateInCheck()
 	return true;
 }
 
-bool MoveValidator::isPathClear(Position from, Position to, PieceType piece, Color color, Game &game)
+Bitboard MoveValidator::generatePotentialMoves(Position position, PieceType piece, Color color, Board &board)
+{
+	Bitboard moves = board.getAttacks(piece, color, Utility::calculateSquareNumber(position));
+
+	switch (piece)
+	{
+	case PieceType::PAWN:
+		return generatePotentialPawnMoves(color, position, moves);
+	case PieceType::KING:
+		return generatePotentialKingMoves(color, position, moves);
+	case PieceType::KNIGHT:
+	case PieceType::BISHOP:
+	case PieceType::ROOK:
+	case PieceType::QUEEN:
+		return moves;
+	default:
+		return 0;
+	}
+}
+
+Bitboard MoveValidator::generatePotentialPawnMoves(Color color, Position position, Bitboard moves)
+{
+	int direction = color == Color::WHITE ? -1 : 1;
+
+	Position singleMove{position.row + direction, position.col};
+	Position doubleMove{position.row + 2 * direction, position.col};
+
+	if (singleMove.row >= 0 && singleMove.row < 8 && singleMove.col >= 0 && singleMove.col < 8)
+	{
+		moves.setBit(singleMove);
+	}
+
+	if (canDoublePawnPush(color, position) && doubleMove.row >= 0 && doubleMove.row < 8 && doubleMove.col >= 0 && doubleMove.col < 8)
+	{
+		moves.setBit(doubleMove);
+	}
+
+	return moves;
+}
+
+bool MoveValidator::canDoublePawnPush(Color color, Position position)
+{
+	return (color == Color::WHITE && position.row == 6) || (color == Color::BLACK && position.row == 1);
+}
+
+Bitboard MoveValidator::generatePotentialKingMoves(Color color, Position position, Bitboard moves)
+{
+	// Doesnt consider board state, only theoretically potential moves
+	if (position.col == 4)
+	{
+		if (position.row == 0 && color == Color::BLACK)
+		{
+			moves.setBit(Position{0, 6});
+			moves.setBit(Position{0, 2});
+		}
+		else if (position.row == 7 && color == Color::WHITE)
+		{
+			moves.setBit(Position{7, 6});
+			moves.setBit(Position{7, 2});
+		}
+	}
+
+	return moves;
+}
+
+bool MoveValidator::isPathClear(Position from, Position to, PieceType piece, Color color, Board &board)
 {
 	Bitboard path = calculatePath(from, to, piece, color);
-	Bitboard occupied = game.getBoard().getOccupiedBitboard();
+	Bitboard occupied = board.getOccupiedBitboard();
 
 	// Check if the path is clear by checking if the intersection of the path and the occupied squares is empty
 	return (path & occupied) == Bitboard();
@@ -196,7 +262,7 @@ bool MoveValidator::isOrthogonal(Position from, Position to)
 	return to.row == from.row || to.col == from.col;
 }
 
-void MoveValidator::validatePawnMove(Position from, Position to, PieceType piece, Color color, Game &game)
+void MoveValidator::validatePawnMove(Position from, Position to, PieceType piece, Color color, Board &board)
 {
 	Color opponentColor = (color == Color::WHITE) ? Color::BLACK : Color::WHITE;
 
@@ -204,14 +270,14 @@ void MoveValidator::validatePawnMove(Position from, Position to, PieceType piece
 	if (isDiagonal(from, to))
 	{
 		// Check if the pawn is capturing a piece
-		if (game.getBoard().getColorBitboard(opponentColor).getBit(to) != 1)
+		if (board.getColorBitboard(opponentColor).getBit(to) != 1)
 		{
 			// Check if the pawn is on an en passant eligible square
 			int enPassantEligibleRow = (color == Color::WHITE) ? 3 : 4;
 			if (from.row == enPassantEligibleRow)
 			{
 				// if the en passant target square is not set or is not the to square, then the move is invalid
-				if (!game.getBoard().getEnPassantTargetSquare().has_value() || game.getBoard().getEnPassantTargetSquare().value() != to)
+				if (!board.getEnPassantTargetSquare().has_value() || board.getEnPassantTargetSquare().value() != to)
 				{
 					throw std::invalid_argument("Invalid move - Pawn can only capture en passant if the opponents previous move was a pawn push of 2 squares that landed adjacent to this pawn");
 				}
@@ -225,7 +291,7 @@ void MoveValidator::validatePawnMove(Position from, Position to, PieceType piece
 	}
 	else
 	{
-		if (game.getBoard().getColorBitboard(opponentColor).getBit(to) != 0)
+		if (board.getColorBitboard(opponentColor).getBit(to) != 0)
 		{
 			throw std::invalid_argument("Invalid move - You cannot capture a piece by moving forward");
 		}
@@ -245,7 +311,7 @@ void MoveValidator::validateKingMove(Position from, Position to, PieceType piece
 	else
 	{
 		// Check if the king is moving into check
-		if (isSquareAttacked(to, color, game))
+		if (isSquareAttacked(to, color, game.getBoard()))
 		{
 			throw std::invalid_argument("Invalid move - The king cannot move into check");
 		}
@@ -269,17 +335,19 @@ void MoveValidator::validateCastlingMove(Position from, Position to, Color color
 			throw std::invalid_argument("Invalid move - The king cannot castle queenside");
 		}
 
+		Board &board = game.getBoard();
+
 		// Check if the squares between the king and the rook are empty
 		int direction = (to.col > from.col) ? 1 : -1;
 		for(int i = from.col + direction; (i < 7 && i > 0); i += direction)
 		{
-			if (game.getBoard().getOccupiedBitboard().getBit(Position{from.row, i}) != 0)
+			if (board.getOccupiedBitboard().getBit(Position{from.row, i}) != 0)
 			{
 				throw std::invalid_argument("Invalid move - The path between the king and the rook is not clear");
 			}
 
 			// Check if the king is castling through check
-			if (isSquareAttacked(Position{from.row, i}, color, game))
+			if (isSquareAttacked(Position{from.row, i}, color, board))
 			{
 				throw std::invalid_argument("Invalid move - The king cannot castle through check");
 			}
@@ -291,10 +359,10 @@ void MoveValidator::validateCastlingMove(Position from, Position to, Color color
 	}
 }
 
-bool MoveValidator::isSquareAttacked(Position position, Color color, Game &game)
+bool MoveValidator::isSquareAttacked(Position position, Color color, Board &board)
 {
 	Color opponentColor = (color == Color::WHITE) ? Color::BLACK : Color::WHITE;
-	Bitboard opponentPieces = game.getBoard().getColorBitboard(opponentColor);
+	Bitboard opponentPieces = board.getColorBitboard(opponentColor);
 
 	for (int square = 0; square < 64; square++)
 	{
@@ -304,20 +372,20 @@ bool MoveValidator::isSquareAttacked(Position position, Color color, Game &game)
 			for (PieceType piece : {PieceType::PAWN, PieceType::KNIGHT, PieceType::BISHOP, PieceType::ROOK, PieceType::QUEEN, PieceType::KING})
 			{
 				// Check if the opponent piece is on the square
-				if (game.getBoard().getPieceBitboard(piece, opponentColor).getBit(Utility::calculatePosition(square)) == 0)
+				if (board.getPieceBitboard(piece, opponentColor).getBit(Utility::calculatePosition(square)) == 0)
 				{
 					continue;
 				}
 
 				// Check if the opponent piece can attack the square
-				Bitboard attacks = game.getBoard().getAttacks(piece, opponentColor, square);
+				Bitboard attacks = board.getAttacks(piece, opponentColor, square);
 				if (attacks.getBit(position) == true)
 				{
 					// Check if the path is clear
 					if (piece != PieceType::KNIGHT && piece != PieceType::KING)
 					{
 						Bitboard path = calculatePath(Utility::calculatePosition(square), position, piece, opponentColor);
-						Bitboard occupied = game.getBoard().getOccupiedBitboard();
+						Bitboard occupied = board.getOccupiedBitboard();
 
 						if ((path & occupied) == Bitboard())
 						{
